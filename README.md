ee641e3b6695e639b50c44e0a5a1c0361e9a9a85
JonLuca DeCaro <jdecaro@usc.edu>
2019-07-06 14:58:53 -0700

    Language codes, functionality

--- README.md
+++ README.md
@@ -4,4 +4,31 @@ Given a top level `messages.json`, this extension will attempt to create a `_loc
 
 ## Usage
 
-``
+```text
+Usage: translator.py [OPTIONS]
+
+Options:
+  --in-lang TEXT           Language you are translating from.
+  --out-lang TEXT          CSV of languages you'd like to translate to,
+                           default to all langues:
+                           ar,am,bg,bn,ca,cs,da,de,el,
+                           en,en_GB,en_US,es,es_419,et,fa,fi,fil,fr,gu,he,hi,h
+                           r,hu,id,it,ja,kn,ko,lt,lv,ml,mr,ms,nl,no,pl,pt_BR,p
+                           t_PT,ro,ru,sk,sl,sr,sv,sw,ta,te,th,tr,uk,vi,zh_CN,z
+                           h_TW
+  --file TEXT              Path to your messages.json file you want translated
+  --remove-locale BOOLEAN  Should remove the existing locales folder
+  --help                   Show this message and exit.
+```
+
+## Sample
+
+```bash
+python translator.py --in-lang=en --out-lang=en,es,it,zh_CN,fr --remove-locale=false
+```
+
+## Important Notes
+
+If you start getting "Error translating language: LANG - skipping", it most likely means that you are being rate limited by Google.
+
+Either switch to a VPN/set up some rotating proxies or wait it out. 
--- translator.py
+++ translator.py
@@ -9,12 +9,15 @@ from googletrans import Translator
 
 from lang_codes import *
 
+chrome_langs_csv = ','.join(chrome_langs)
+
 
 @click.command()
 @click.option('--in-lang', default='en', help='Language you are translating from.')
-@click.option('--out-lang', default='en,es', help='CSV of languages you are going to')
+@click.option('--out-lang', default=chrome_langs_csv,
+              help='CSV of languages you\'d like to translate to, default to all langues:\n' + chrome_langs_csv)
 @click.option('--file', default='messages.json', help='Path to your messages.json file you want translated')
-@click.option('--remove-locale', default=True, help='Should remove the existing locales folder')
+@click.option('--remove-locale', default=True, type=bool, help='Should remove the existing locales folder')
 def translate(in_lang, out_lang, file, remove_locale):
     translator = Translator()
     messages = json.loads(open(file).read())
@@ -23,21 +26,41 @@ def translate(in_lang, out_lang, file, remove_locale):
 
     # Generate dictionary of all languages requested
     for lang in langs:
+        lang_map = mapping_chrome_langs_to_google_translate[lang]
         if lang not in chrome_langs:
-            sys.exit('Selected output language not accepted by Chrome!')
-        if lang not in google_translate_langs:
-            sys.exit('Selected language cannot be translated by Google Chrome!')
+            sys.exit(f'Selected output language {lang} not accepted by Chrome!')
+        if lang_map not in google_translate_langs:
+            sys.exit(f'Selected language {lang} cannot be translated by Google Chrome!')
         translations[lang] = {}
 
-    for key in messages.keys():
-        for lang in langs:
-            # if we're translating to the same language, skip it
+    for lang in langs:
+        try:
+            # if we're translating to the same language, save it and then skip it
             if lang == in_lang:
+                translations[lang] = messages
                 continue
-            actual_string = messages[key]['message']
-            trans_string = translator.translate(actual_string, src=in_lang, dest=lang)
-            translations[lang][key] = {'message': trans_string.text}
-    write_translations(translations, remove_locale)
+            click.echo(f'Translating {lang}')
+            for key in messages.keys():
+                actual_string = messages[key]['message']
+                # the short codes between GTrans and GChrome are different, so dest should be the mapping we know
+                lang_map = mapping_chrome_langs_to_google_translate[lang]
+                trans_string = translator.translate(actual_string, src=in_lang, dest=lang_map)
+                translations[lang][key] = {'message': trans_string.text}
+            write_translations(translations, remove_locale)
+        except Exception as e:
+            click.echo(f'Error translating language: {lang} - skipping')
+            print(e)
+    click.echo("""
+    Done!
+    
+    Note: The saved _locales folder might not have the correct folder names. Verify and rename the appropriately according to this site:
+    
+    https://developer.chrome.com/webstore/i18n?csw=1#localeTable
+    
+    That the locale is named with the appropriate short code. 
+    
+    This is due to slight differences between the translation short codes used by google translate and google chrome.
+    """)
 
 
 def write_translations(translations, remove_locale):
@@ -48,8 +71,11 @@ def write_translations(translations, remove_locale):
     for lang in translations.keys():
         if not os.path.isdir('_locales/' + lang):
             os.mkdir('_locales/' + lang)
-        with codecs.open('_locales/' + lang + '/messages.json', 'w', encoding='utf-8') as outfile:
-            json.dump(translations[lang], outfile, ensure_ascii=False)
+        try:
+            with codecs.open('_locales/' + lang + '/messages.json', 'w', encoding='utf-8') as outfile:
+                json.dump(translations[lang], outfile, ensure_ascii=False)
+        except Exception as e:
+            print(f'Error writing {lang}: {e}')
 
 
 if __name__ == '__main__':
